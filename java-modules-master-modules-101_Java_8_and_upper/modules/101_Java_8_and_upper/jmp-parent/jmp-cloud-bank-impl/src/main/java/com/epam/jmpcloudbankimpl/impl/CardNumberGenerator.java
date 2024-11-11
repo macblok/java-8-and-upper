baca2a1291211ada1d12529d38124f53159ca29a
Maciej Blok <maciekblok@gmail.com>
2024-11-11 14:38:48 +0100

    Add javadoc documentation

--- java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-bank-impl/src/main/java/com/epam/jmpcloudbankimpl/impl/CardNumberGenerator.java
+++ java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-bank-impl/src/main/java/com/epam/jmpcloudbankimpl/impl/CardNumberGenerator.java
@@ -6,6 +6,14 @@ class CardNumberGenerator {
     private static final AtomicLong cardNumberCounter = new AtomicLong(1111111111111111L);
     private static final String CARD_NUMBER_FORMAT = "%04d-%04d-%04d-%04d";
 
+    /**
+     * Generates a globally unique bank card number using an atomic counter.
+     * The method increments the counter each time it is invoked to ensure that every card number generated is distinct.
+     * The card number is formatted according to a pre-defined format: {4 digits}-{4 digits}-{4 digits}-{4 digits}.
+     * The initial value of the counter is set to make the first generated number "1111-1111-1111-1111".
+     *
+     * @return A formatted bank card number as a String.
+     */
     String generateCardNumber() {
         var currentNumber = cardNumberCounter.getAndIncrement();
         return formatCardNumber(currentNumber);
--- java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-service-impl/src/main/java/com/epam/jmpcloudserviceimpl/impl/ServiceImpl.java
+++ java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-service-impl/src/main/java/com/epam/jmpcloudserviceimpl/impl/ServiceImpl.java
@@ -7,14 +7,21 @@ import com.epam.jmpserviceapi.service.Service;
 
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 public class ServiceImpl implements Service {
 
     //Simulating database with HashMap
-    private final Map<String, Subscription> subscriptionDatabase = new HashMap<String, Subscription>();
+    private final Map<String, Subscription> subscriptionDatabase = new HashMap<>();
 
+    /**
+     * Subscribes a {@link BankCard} to the banking service. This involves validating the bank card, checking if
+     * a subscription already exists, and then storing the new subscription initialized with the current date.
+     *
+     * @param bankCard The bank card to subscribe (must not be null).
+     * @throws IllegalArgumentException if the provided bank card is null.
+     * @throws IllegalStateException if a subscription for the bank card already exists.
+     */
     @Override
     public void subscribe(BankCard bankCard) {
         if (bankCard == null) {
@@ -30,6 +37,13 @@ public class ServiceImpl implements Service {
         subscriptionDatabase.put(bankCard.getNumber(), subscription);
     }
 
+    /**
+     * Retrieves a subscription based on a provided card number.
+     *
+     * @param cardNumber The card number of the subscription to retrieve (must not be null, empty, or invalid).
+     * @return An {@link Optional} containing the subscription if found, or an empty Optional if no subscription is found.
+     * @throws IllegalArgumentException if the card number is null, empty, or does not meet valid length criteria.
+     */
     @Override
     public Optional<Subscription> getSubscriptionByBankCardNumber(String cardNumber) {
         if (cardNumber == null || cardNumber.trim().isEmpty() || cardNumber.length() != 19) {
@@ -41,9 +55,18 @@ public class ServiceImpl implements Service {
         return Optional.ofNullable(foundSubscription);
     }
 
+    /**
+     * Fetches all distinct users associated with any subscription in the banking service.
+     * This method collects all users from the stored subscriptions, ensuring each user is distinct. The resulting list
+     * is unmodifiable, meaning that you cannot perform operations like add, remove, or clear on this list.
+     *
+     * @return An unmodifiable list of {@link User} representing all unique users who have subscriptions.
+     */
     @Override
     public List<User> getAllUsers() {
-        return subscriptionDatabase.values().stream().
-                map(Subscription::getCardUser).filter(Objects::nonNull).distinct().collect(Collectors.toUnmodifiableList());
+        return subscriptionDatabase.values().stream()
+                .map(Subscription::getCardUser)
+                .filter(Objects::nonNull)
+                .distinct().toList();
     }
 }
