c15e39ec3c5a823bac568c92d0601cfae0ef489c
Maciej Blok <maciekblok@gmail.com>
2024-11-11 10:38:31 +0100

    Implement ServiceImpl

--- java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-service-impl/src/main/java/com/epam/jmpcloudserviceimpl/impl/ServiceImpl.java
+++ java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-service-impl/src/main/java/com/epam/jmpcloudserviceimpl/impl/ServiceImpl.java
@@ -5,24 +5,45 @@ import com.epam.jmpdto.dto.Subscription;
 import com.epam.jmpdto.dto.User;
 import com.epam.jmpserviceapi.service.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class ServiceImpl implements Service {
+
+    //Simulating database with HashMap
+    private final Map<String, Subscription> subscriptionDatabase = new HashMap<>();
+
     @Override
     public void subscribe(BankCard bankCard) {
+        if (bankCard == null) {
+            throw new IllegalArgumentException("Bank card must not be null");
+        }
 
+        if (subscriptionDatabase.containsKey(bankCard.getNumber())) {
+            throw new IllegalStateException("Subscription already exists for this card number!");
+        }
+
+        Subscription subscription = new Subscription(bankCard, LocalDate.now());
+
+        subscriptionDatabase.put(bankCard.getNumber(), subscription);
     }
 
     @Override
     public Optional<Subscription> getSubscriptionByBankCardNumber(String cardNumber) {
-        return Optional.empty();
+        if (cardNumber == null || cardNumber.trim().isEmpty() || cardNumber.length() != 19) {
+            throw new IllegalArgumentException("Card number cannot be null, empty or invalid.");
+        }
+
+        Subscription foundSubscription = subscriptionDatabase.get(cardNumber);
+
+        return Optional.ofNullable(foundSubscription);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return Collections.EMPTY_LIST;
+        return subscriptionDatabase.values().stream().
+                map(Subscription::getCardUser).filter(Objects::nonNull).distinct().collect(Collectors.toList());
     }
 }
