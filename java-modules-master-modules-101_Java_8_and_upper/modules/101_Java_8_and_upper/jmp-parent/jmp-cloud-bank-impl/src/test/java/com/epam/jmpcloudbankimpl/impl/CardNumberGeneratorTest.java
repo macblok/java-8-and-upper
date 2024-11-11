a06e642d356fa2480cc335b10348472a5f4e028f
Maciej Blok <maciekblok@gmail.com>
2024-11-11 15:33:13 +0100

    Add CardNumberGenerator tests

---
+++ java-modules-master-modules-101_Java_8_and_upper/modules/101_Java_8_and_upper/jmp-parent/jmp-cloud-bank-impl/src/test/java/com/epam/jmpcloudbankimpl/impl/CardNumberGeneratorTest.java
@@ -0,0 +1,32 @@
+package com.epam.jmpcloudbankimpl.impl;
+
+import junit.framework.TestCase;
+
+public class CardNumberGeneratorTest extends TestCase {
+
+    private CardNumberGenerator cardNumberGenerator;
+
+    protected void setUp() {
+        cardNumberGenerator = new CardNumberGenerator();
+    }
+
+    public void testGenerateCardNumberUnique() {
+        String firstCardNumber = cardNumberGenerator.generateCardNumber();
+        String secondCardNumber = cardNumberGenerator.generateCardNumber();
+        assertNotNull("First card number should not be null", firstCardNumber);
+        assertNotNull("Second card number should not be null", secondCardNumber);
+        assertFalse("Card numbers should be unique", firstCardNumber.equals(secondCardNumber));
+    }
+
+    public void testGenerateCardNumberFormat() {
+        String cardNumber = cardNumberGenerator.generateCardNumber();
+        assertNotNull("Card number should not be null", cardNumber);
+        assertTrue("Card number should match the required format", cardNumber.matches("\\d{4}-\\d{4}-\\d{4}-\\d{4}"));
+    }
+
+    public void testGenerateCardNumberSequentialIncrement() {
+        long firstNumber = Long.parseLong(cardNumberGenerator.generateCardNumber().replace("-", ""));
+        long secondNumber = Long.parseLong(cardNumberGenerator.generateCardNumber().replace("-", ""));
+        assertEquals("The second number should be exactly one greater than the first number", firstNumber + 1, secondNumber);
+    }
+}
